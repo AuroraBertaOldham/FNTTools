025e87d3f69eb4d4dbdff75fb5e432717269f39c
Todd Berta-Oldham <16536474+ToddBertaOldham@users.noreply.github.com>
2019-01-10 05:32:57 -0500

    Add project files.

---
+++ FNTTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FNTTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
